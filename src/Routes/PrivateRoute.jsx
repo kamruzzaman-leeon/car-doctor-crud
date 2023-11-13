29c0db1782bb1b274cfc34576b86737fe65cddd6
kamruzzaman-leeon <leeon.ewubd@gmail.com>
2023-11-13 21:34:42 +0600

    git cache cleared

--- src/Routes/PrivateRoute.jsx
+++ src/Routes/PrivateRoute.jsx
@@ -13,7 +13,7 @@ const PrivateRoute = ({children}) => {
         return children;
     }
 
-    return <Navigate to="/login" replace></Navigate>;
+    return <Navigate state={location.pathname} to="/login" replace></Navigate>;
 };
 
 export default PrivateRoute;
